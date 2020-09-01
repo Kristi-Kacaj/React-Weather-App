358eb0d148c479886931a2bee5212579314a8088
Julian Villalba <62047235+jrvillalba1993@users.noreply.github.com>
2020-09-01 13:53:54 -0400

    yezzir

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
-
 import './components/App.css';
-
 import {  Main } from './components';
 import {  Head } from './components';
 import {  Footer } from './components';
+
 
 class App extends Component {
 
 
   render() {
     return (
+      
       <div >
 
         <Head />
         <Main />
         <Footer />
+        
 
       </div>
+
+      
     );
   }
 }
