9d9644a4c76ebc792c9f4509bb3a5613e0ebb61a
Julian Villalba <62047235+jrvillalba1993@users.noreply.github.com>
2020-09-02 02:35:52 -0400

    another update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,20 +4,34 @@ import {  Main } from './components';
 import {  Head } from './components';
 import {  Footer } from './components';
 
+import "bootstrap/dist/css/bootstrap.css";
+
+
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      comments: [],
+      loading: false
+    };
+  }
 
 
   render() {
+    const loadingSpin = this.state.loading ? "App-logo Spin" : "App-logo";
     return (
       
-      <div >
+      <div>
 
         <Head />
         <Main />
-        <Footer />
-        
 
+        <Footer/>
+        
       </div>
 
       
