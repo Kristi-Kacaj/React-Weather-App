9b561cacea99ad59758790445dc422d0fae8a9b1
Kristi Kacaj <kristik.web@gmail.com>
2020-09-14 19:30:09 -0400

    geocoding function

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import { Form } from './components/Form/form.js';
 
 class App extends React.Component {  
     state = {
+      latitude:undefined,
+      longitude:undefined,
     temperature: undefined,
     city: undefined,
     feels_like: undefined,
@@ -20,8 +22,52 @@ class App extends React.Component {
     description: undefined,
     error: undefined
   }
+
+  componentDidMount = async ()=>{
+    console.log("component mounted")
+
+    if(navigator.geolocation){
+       navigator.geolocation.getCurrentPosition(success=>{
+         console.log(success)
+         const {latitude, longitude} = success.coords
+         this.setState({latitude, longitude})
+         this.getWeatherAuto(latitude, longitude)
+
+       })
+    }
+  }
+
+  getWeatherAuto = async (lat,lon)=>{
+    const API_KEY = "011d246d500f6dadbdd8b82cb9613fc3";
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    if (true) {
+      this.setState({
+        temperature: data.main.temp.toFixed(0),
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        wind: data.wind.speed.toFixed(0),
+        icon: data.weather[0].icon,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        wind: undefined,
+        icon: undefined,        
+        error: "Please enter the values."
+      });
+    }
+  }
   
-  getWeather = async (e) => {
+  
+  getWeatherForm = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
@@ -55,7 +101,7 @@ class App extends React.Component {
   render() {
     return (
       <div >
-
+        <h1>{this.state.latitude} , {this.state.longitude}</h1>
         <Head />
         <Main /> 
         <Form getWeather={this.getWeather} />
