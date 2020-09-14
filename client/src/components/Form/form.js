64210581638abd4e58a3c52a3380159bb8cd6a82
Kaiden Munsey <kaidenmunsey7@gmail.com>
2020-09-13 21:55:28 -0400

    adding function to render icons

--- client/src/components/Form/form.js
+++ client/src/components/Form/form.js
@@ -4,9 +4,9 @@ import './form.css';
 
 const Form = props => (
 	<form onSubmit={props.getWeather}>
-		<input type="text" name="city" placeholder="City..."/>
-		<input type="text" name="country" placeholder="Country..."/>
-		<button>Get Weather</button>
+		<input style={{backgroundColor: "rgba(255,165,0, 0.8", height: "45px", color: "black"}} type="text" name="city" placeholder="City..."/>
+		<input style={{backgroundColor: "rgba(255,165,0, 0.8", height: "45px"}} type="text" name="country" placeholder="Country..."/>
+		<button id="btn">Get Weather</button>
 	</form>
 );
 
